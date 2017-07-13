92fbd337abc5b1909747d9978acfbebfc3c794fe
Randy McClure <rmcclure@student.neumont.edu>
2017-07-13 16:13:38 -0600

    Completed the CanvasDrawDemo
    The project now contains nearly identical code to the CanvasDrawDemo. It works and shows a canvas, but it needs to be rewritten to be our own code.

--- MAIKUPaintapp/app/src/main/java/maiku/maikupaintapp/MainActivity.java
+++ MAIKUPaintapp/app/src/main/java/maiku/maikupaintapp/MainActivity.java
@@ -2,12 +2,21 @@ package maiku.maikupaintapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CanvasView customCanvas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        customCanvas = (CanvasView) findViewById(R.id.canvas);
+    }
+
+    public void clearButtonClick(View view){
+        customCanvas.clearCanvas();
     }
 }
